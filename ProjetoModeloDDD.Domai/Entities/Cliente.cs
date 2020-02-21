c16025ffc266056dcdb2a6e2add16cff92827241
Mathues.Araujo <matheus.araujo@orsoftware.com.br>
2020-02-21 08:52:41 -0300

    Criando estrutura e instalando EntityFramework

---
+++ ProjetoModeloDDD.Domai/Entities/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoModeloDDD.Domain.Entities
+{
+    public class Cliente
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public DateTime DateCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        public bool ClienteEspecial(Cliente cliente)
+        {
+            return cliente.Ativo && DateTime.Now.Year - cliente.DateCadastro.Year >= 5; 
+        }
+    }
+}
